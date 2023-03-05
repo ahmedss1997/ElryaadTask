263f15ed0af1e911414750b097b8a8ca18ad0dd1
ahmedss1997 <asami6596@gmail.com>
2023-03-05 02:05:14 +0200

    modify

--- src/store/data.ts
+++ src/store/data.ts
@@ -1,323 +1,333 @@
 export const data = [
   {
-    "id": 1,
-    "questionText":"A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
-    "rightAnswers":[1,2], 
-    "options": [
+    id: 1,
+    questionText:
+      "A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
+    rightAnswers: [1, 2],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
     ],
-    "table": true
+    table: true,
   },
   {
-    "id": 2,
-    "questionText":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    "rightAnswers":[3], 
-    "options": [
+    id: 2,
+    questionText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    rightAnswers: [3],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
   },
   {
-    "id": 3,
-    "questionText":"A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
-    "rightAnswers":[4,2], 
-    "options": [
+    id: 3,
+    questionText:
+      "A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
+    rightAnswers: [4, 2],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
   },
   {
-    "id": 4,
-    "questionText":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    "rightAnswers":[2], 
-    "options": [
+    id: 4,
+    questionText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    rightAnswers: [2],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
   },
   {
-    "id": 5,
-    "questionText":"A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
-    "rightAnswers":[4,2], 
-    "options": [
+    id: 5,
+    questionText:
+      "A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
+    rightAnswers: [4, 2],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
   },
   {
-    "id": 6,
-    "questionText":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    "rightAnswers":[2], 
-    "options": [
+    id: 6,
+    questionText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    rightAnswers: [2],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
   },
   {
-    "id": 7,
-    "questionText":"A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
-    "rightAnswers":[5,2], 
-    "options": [
+    id: 7,
+    questionText:
+      "A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
+    rightAnswers: [5, 2],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
   },
   {
-    "id": 8,
-    "questionText":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    "rightAnswers":[3,5], 
-    "options": [
+    id: 8,
+    questionText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    rightAnswers: [3, 5],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
   },
   {
-    "id": 9,
-    "questionText":"A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
-    "rightAnswers":[1,2], 
-    "options": [
+    id: 9,
+    questionText:
+      "A 24-Year-Old Student Returns From A Gap Year In Malawi Complaining Of Visible Haematuria, Dysuria And Urinary Frequency. She Says She Felt Well Throughout Her Trip But Experienced An Itchy Rash On Her Legs A Few Hours After Swimming In Lake Malawi Which Has Now Resolved.",
+    rightAnswers: [1, 2],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
   },
   {
-    "id": 10,
-    "questionText":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-    "rightAnswers":[3,4,5], 
-    "options": [
+    id: 10,
+    questionText:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    rightAnswers: [3, 4, 5],
+    options: [
       {
-        "id": 1,
-        "text": "Answer 1",
-        "perc": 7
+        id: 1,
+        text: "Answer 1",
+        perc: 7,
       },
       {
-        "id": 2,
-        "text": "Answer 2",
-        "perc":5
+        id: 2,
+        text: "Answer 2",
+        perc: 5,
       },
       {
-        "id": 3,
-        "text": "Answer 3",
-        "perc": 30
+        id: 3,
+        text: "Answer 3",
+        perc: 30,
       },
       {
-        "id": 4,
-        "text": "Answer 4",
-        "perc": 40
+        id: 4,
+        text: "Answer 4",
+        perc: 40,
       },
       {
-        "id": 5,
-        "text": "Answer 5",
-        "perc": 80
-      }
-    ]
-  }
+        id: 5,
+        text: "Answer 5",
+        perc: 80,
+      },
+    ],
+  },
 ] as any[];
